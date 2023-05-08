3074946e8b63e7139c2cc3a2e8902061a52de2f0
deeepak-me <deepak.krishnan.e@gmail.com>
2023-05-08 18:42:45 +0530

    refactor: user and product data

--- src/formSource.js
+++ src/formSource.js
@@ -1,76 +1,76 @@
 export const userInputs = [
-    {
-        id:1,
-        label:"Username",
-        type:"text",
-        placeholder:"John_Doe",
-    },
-    {
-        id:2,
-        label:"Name and Surnam",
-        type:"text",
-        placeholder:"John Doe",
-    },
-    {
-        id:3,
-        label:"Email",
-        type:"email",
-        placeholder:"johndoe@gmail.com",
-    },
-    {
-        id:4,
-        label:"Phone",
-        type:"text",
-        placeholder:"+91 335656454",
-    },
-    {
-        id:5,
-        label:"Password",
-        type:"password",
-    },
-    {
-        id:6,
-        label:"Address",
-        type:"text",
-        placeholder:"Street no:1, NewYork",
-    },
-    {
-        id:7,
-        label:"Country",
-        type:"text",
-        placeholder:"USA",
-    },
+  {
+    id: 1,
+    label: "Username",
+    type: "text",
+    placeholder: "John_Doe",
+  },
+  {
+    id: 2,
+    label: "Name and Surnam",
+    type: "text",
+    placeholder: "John Doe",
+  },
+  {
+    id: 3,
+    label: "Email",
+    type: "email",
+    placeholder: "johndoe@gmail.com",
+  },
+  {
+    id: 4,
+    label: "Phone",
+    type: "text",
+    placeholder: "+91 335656454",
+  },
+  {
+    id: 5,
+    label: "Password",
+    type: "password",
+  },
+  {
+    id: 6,
+    label: "Address",
+    type: "text",
+    placeholder: "Street no:1, NewYork",
+  },
+  {
+    id: 7,
+    label: "Country",
+    type: "text",
+    placeholder: "USA",
+  },
 ];
 
-export const productInput=[
-    {
-        id:1,
-        label:"Title",
-        type:"text",
-        placeholder:"Apple Macbook Pro",
-    },
-    {
-        id:2,
-        label:"Description",
-        type:"text",
-        placeholder:"Description",
-    },
-    {
-        id:3,
-        label:"Category",
-        type:"text",
-        placeholder:"Computers",
-    },
-    {
-        id:1,
-        label:"Price",
-        type:"text",
-        placeholder:"100",
-    },
-    {
-        id:5,
-        label:"Stock",
-        type:"text",
-        placeholder:"in stock",
-    },
+export const productInput = [
+  {
+    id: 1,
+    label: "Title",
+    type: "text",
+    placeholder: "Apple Macbook Pro",
+  },
+  {
+    id: 2,
+    label: "Description",
+    type: "text",
+    placeholder: "Description",
+  },
+  {
+    id: 3,
+    label: "Category",
+    type: "text",
+    placeholder: "Computers",
+  },
+  {
+    id: 1,
+    label: "Price",
+    type: "text",
+    placeholder: "100",
+  },
+  {
+    id: 5,
+    label: "Stock",
+    type: "text",
+    placeholder: "in stock",
+  },
 ];
