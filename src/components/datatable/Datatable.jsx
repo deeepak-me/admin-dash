242951359a2169d52098c1fd5f0586ed09c461d5
deeepak-me <deepak.krishnan.e@gmail.com>
2023-05-09 16:26:15 +0530

    feat: Added a add new button

--- src/components/datatable/Datatable.jsx
+++ src/components/datatable/Datatable.jsx
@@ -1,35 +1,45 @@
-import "./datatable.scss"
-import { DataGrid } from '@mui/x-data-grid';
-import { userColumn,userRows } from "../../datatablesource";
-
-
+import "./datatable.scss";
+import { DataGrid } from "@mui/x-data-grid";
+import { userColumn, userRows } from "../../datatablesource";
+import { Link } from "react-router-dom";
 
 const Datatable = () => {
-
-  const actionColumn=[
-    {field:"action",headerName:"Action",width:200,
-      renderCell:()=>{
-        return(
+  const actionColumn = [
+    {
+      field: "action",
+      headerName: "Action",
+      width: 200,
+      renderCell: () => {
+        return (
           <div className="cellAction">
-            <div className="viewButton">View</div>
+            <Link to="/users/test" style={{ textDecoration: "none" }}>
+              <div className="viewButton">View</div>
+            </Link>
+
             <div className="deleteButton">Delete</div>
           </div>
-        )
-      }
-    }
-  ]
+        );
+      },
+    },
+  ];
 
   return (
     <div className="dataTable">
-        <DataGrid
-            rows={userRows}
-            columns={userColumn.concat(actionColumn)}
-            pageSize={9}
-            rowsPerPageOptions={[9]}
-            checkboxSelection
+      <div className="datatableTitle">
+        Add New User
+        <Link to="/users/new" className="link">
+          Add New
+        </Link>
+      </div>
+      <DataGrid
+        rows={userRows}
+        columns={userColumn.concat(actionColumn)}
+        pageSize={9}
+        rowsPerPageOptions={[9]}
+        checkboxSelection
       />
     </div>
-  )
-}
+  );
+};
 
-export default Datatable
+export default Datatable;
