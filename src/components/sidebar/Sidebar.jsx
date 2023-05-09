3d66099888951a1f0923f554fe4e4c234bef4b8e
deeepak-me <deepak.krishnan.e@gmail.com>
2023-05-09 16:26:50 +0530

    feat: adding router links

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,86 +1,95 @@
-import "./sidebar.scss"; 
-import DashboardIcon from '@mui/icons-material/Dashboard';
-import Person2OutlinedIcon from '@mui/icons-material/Person2Outlined';
-import StoreIcon from '@mui/icons-material/Store';
-import InventoryIcon from '@mui/icons-material/Inventory';
-import LocalShippingIcon from '@mui/icons-material/LocalShipping';
-import AssessmentIcon from '@mui/icons-material/Assessment';
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import SettingsSystemDaydreamIcon from '@mui/icons-material/SettingsSystemDaydream';
-import PsychologyIcon from '@mui/icons-material/Psychology';
-import SettingsIcon from '@mui/icons-material/Settings';
-import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
-import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
-
+import "./sidebar.scss";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import Person2OutlinedIcon from "@mui/icons-material/Person2Outlined";
+import StoreIcon from "@mui/icons-material/Store";
+import InventoryIcon from "@mui/icons-material/Inventory";
+import LocalShippingIcon from "@mui/icons-material/LocalShipping";
+import AssessmentIcon from "@mui/icons-material/Assessment";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import SettingsSystemDaydreamIcon from "@mui/icons-material/SettingsSystemDaydream";
+import PsychologyIcon from "@mui/icons-material/Psychology";
+import SettingsIcon from "@mui/icons-material/Settings";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import LogoutOutlinedIcon from "@mui/icons-material/LogoutOutlined";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
-        <div className="top"><span className="logo">JD</span></div>
-        <hr />
-        <div className="center">
-            <ul>
-                <p className="title">MAIN</p>
-                <li>
-                    <DashboardIcon className="icon"/>
-                    <span>Dashboard</span>
-                </li>
-                <p className="title">LISTS</p>
-                <li>
-                    <Person2OutlinedIcon className="icon"/>
-                    <span>Users</span>
-                </li>
-                <li>
-                    <StoreIcon className="icon"/>
-                    <span>Products</span>
-                </li>
-                <li>
-                    <InventoryIcon className="icon"/>
-                    <span>Orders</span>
-                </li>
-                <li>
-                    <LocalShippingIcon className="icon"/>
-                    <span>Delivery</span>
-                </li>
-                <p className="title">USEFUL</p>
-                <li>
-                    <AssessmentIcon className="icon"/>
-                    <span>Stats</span>
-                </li>
-                <li>
-                    <NotificationsIcon className="icon"/>
-                    <span>Notification</span>
-                </li>
-                <p className="title">SERVICE</p>
-                <li>
-                    <SettingsSystemDaydreamIcon className="icon"/>
-                    <span>System Health</span>
-                </li>
-                <li>
-                    <PsychologyIcon className="icon"/>
-                    <span>Logs</span>
-                </li>
-                <li>
-                    <SettingsIcon className="icon"/>
-                    <span>Settings</span>
-                </li>
-                <p className="title">USER</p>
-                <li>
-                    <AccountCircleOutlinedIcon className="icon"/>
-                    <span>Profile</span>
-                </li>
-                <li>
-                    <LogoutOutlinedIcon className="icon"/>
-                    <span>Logout</span>
-                </li>
-            </ul>
-        </div>
-        <div className="bottom">
-            <div className="colorOption"></div>
-            <div className="colorOption"></div>
-        </div>
+      <div className="top">
+        <Link to="/" style={{textDecoration:'none'}}>
+          <span className="logo">JD</span>
+        </Link>
+      </div>
+      <hr />
+      <div className="center">
+        <ul>
+          <p className="title">MAIN</p>
+          <li>
+            <DashboardIcon className="icon" />
+            <span>Dashboard</span>
+          </li>
+          <p className="title">LISTS</p>
+          <Link to="/users" style={{textDecoration:'none'}}>
+          <li>
+            <Person2OutlinedIcon className="icon" />
+            <span>Users</span>
+          </li>
+          </Link>
+          <Link to="/products" style={{textDecoration:'none'}}>
+          <li>
+            <StoreIcon className="icon" />
+            <span>Products</span>
+          </li>
+          </Link>
+          
+          <li>
+            <InventoryIcon className="icon" />
+            <span>Orders</span>
+          </li>
+          <li>
+            <LocalShippingIcon className="icon" />
+            <span>Delivery</span>
+          </li>
+          <p className="title">USEFUL</p>
+          <li>
+            <AssessmentIcon className="icon" />
+            <span>Stats</span>
+          </li>
+          <li>
+            <NotificationsIcon className="icon" />
+            <span>Notification</span>
+          </li>
+          <p className="title">SERVICE</p>
+          <li>
+            <SettingsSystemDaydreamIcon className="icon" />
+            <span>System Health</span>
+          </li>
+          <li>
+            <PsychologyIcon className="icon" />
+            <span>Logs</span>
+          </li>
+          <li>
+            <SettingsIcon className="icon" />
+            <span>Settings</span>
+          </li>
+          <p className="title">USER</p>
+          <li>
+            <AccountCircleOutlinedIcon className="icon" />
+            <span>Profile</span>
+          </li>
+          <li>
+            <LogoutOutlinedIcon className="icon" />
+            <span>Logout</span>
+          </li>
+        </ul>
+      </div>
+      <div className="bottom">
+        <div className="colorOption"></div>
+        <div className="colorOption"></div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
