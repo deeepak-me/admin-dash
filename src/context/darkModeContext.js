6ded500de61be264fcffe3c3819603498b014599
deeepak-me <deepak.krishnan.e@gmail.com>
2023-05-13 23:56:06 +0530

    feat: context api file created

---
+++ src/context/darkModeContext.js
@@ -0,0 +1,11 @@
+import { createContext } from "react"
+
+const INITIAL_STATE ={
+    darkMode: false
+}
+
+export const DarkModeContext = createContext(INITIAL_STATE);
+
+export const DarkModeContextProvider = ({children}) =>{
+    
+}
