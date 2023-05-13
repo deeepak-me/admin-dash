73f86bdbe3e9441e0f996c539246bbe615ce8f3f
deeepak-me <deepak.krishnan.e@gmail.com>
2023-05-13 23:55:32 +0530

    refactor: removing dark mode classname

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import "./style/dark.scss";
 
 function App() {
   return (
-    <div className="app dark">
+    <div className="app">
       <BrowserRouter>
         <Routes>
           <Route path="/">
