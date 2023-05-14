521f7f40027e41ab90f2d9ef6a0c6d7ff52f61f8
deeepak-me <deepak.krishnan.e@gmail.com>
2023-05-14 12:45:05 +0530

    feat: dark mode context api created

--- src/context/darkModeContext.js
+++ src/context/darkModeContext.js
@@ -1,11 +1,18 @@
-import { createContext } from "react"
+import { createContext, useReducer } from "react";
+import DarkModeReducer from "./darkModeReducer";
 
-const INITIAL_STATE ={
-    darkMode: false
-}
+const INITIAL_STATE = {
+  darkMode: false,
+};
 
 export const DarkModeContext = createContext(INITIAL_STATE);
 
-export const DarkModeContextProvider = ({children}) =>{
-    
-}
+export const DarkModeContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(DarkModeReducer, INITIAL_STATE);
+
+  return (
+    <DarkModeContext.Provider value={{ darkMode: state.darkMode, dispatch }}>
+      {children}
+    </DarkModeContext.Provider>
+  );
+};
