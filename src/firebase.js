2fcb1abc4ac40becd9bc2f387fea83fc3067006e
deeepak-me <deepak.krishnan.e@gmail.com>
2023-05-25 00:42:56 +0530

    feat: firebase authentication sign in exported

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,11 @@
 
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  apiKey:"AIzaSyBP-kbE5bTAPTGx6DXtv6d2aL0bi-6-Zb0",
+  //apiKey: process.env.REACT_APP_FIREBASE_KEY,
   authDomain: "admin-panel-6bde7.firebaseapp.com",
   projectId: "admin-panel-6bde7",
   storageBucket: "admin-panel-6bde7.appspot.com",
@@ -10,5 +13,8 @@ const firebaseConfig = {
   appId: "1:761715944160:web:bd01acda226707f7ae3e85"
 };
 
+//console.log(process.env);
+
 
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
